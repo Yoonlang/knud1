8bf18446976113a3a8f0b6748611a184324036cb
Climier-code <ikowhddus@naver.com>
2022-08-31 21:20:14 +0900

    feat: add common styled

---
+++ src/components/common/styled/elements.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ButtonBase = styled.button`
+  background: inherit;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  padding: 0;
+  overflow: visible;
+  cursor: pointer;
+`;
