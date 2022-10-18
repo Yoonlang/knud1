1414a3ee715ed010ae04102298345d606c56f8a0
Climier-code <ikowhddus@naver.com>
2022-10-18 22:06:58 +0900

    feat: close when click out of sidebar

--- src/components/common/header/HeaderMobile/index.tsx
+++ src/components/common/header/HeaderMobile/index.tsx
@@ -9,6 +9,7 @@ import { ButtonBase } from 'components/common/styled/elements';
 import { Column, Row } from 'components/common/styled/layout';
 import cx from 'classnames';
 import Image from 'next/future/image';
+import useOutAreaClick from 'utils/useOutAreaClick';
 
 interface Props {
   identity?: boolean;
@@ -17,11 +18,26 @@ interface Props {
 const HeaderMobile = React.forwardRef<HTMLAnchorElement, Props>((props, ref) => {
   const { identity = false } = props;
 
+  const sideBarRef = React.useRef<HTMLDivElement>(null);
+
   const router = useRouter();
 
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const toggleMenuOpen = () => setIsMenuOpen(!isMenuOpen);
 
+  const handleOpenMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsMenuOpen(true);
+  };
+
+  const handleCloseMenu = () => {
+    if (!isMenuOpen) {
+      return;
+    }
+
+    setIsMenuOpen(false);
+  };
+
+  useOutAreaClick(sideBarRef, handleCloseMenu);
   return (
     <HeaderMobileWrapper className={cx({ identity: identity })}>
       <Row className="header-wrap">
@@ -32,20 +48,20 @@ const HeaderMobile = React.forwardRef<HTMLAnchorElement, Props>((props, ref) => 
         </Link>
 
         {!isMenuOpen && (
-          <ButtonBase onClick={toggleMenuOpen} className={'menu-button'}>
+          <ButtonBase onClick={handleOpenMenu} className={'menu-button'}>
             <Image src="/assets/menu.svg" alt="menu" width={32} height={32} placeholder="empty" priority />
           </ButtonBase>
         )}
       </Row>
 
-      <Column className={cx('side-bar', { open: isMenuOpen, close: !isMenuOpen })}>
-        <ButtonBase onClick={toggleMenuOpen} className={'close-button'}>
+      <Column ref={sideBarRef} className={cx('side-bar', { open: isMenuOpen, close: !isMenuOpen })}>
+        <ButtonBase onClick={handleCloseMenu} className={'close-button'}>
           <Image src="/assets/close.svg" alt="close" width={32} height={32} placeholder="empty" priority />
         </ButtonBase>
         {AppPathsArray.map(({ linkText, href }) => {
           return (
             <Link href={href} passHref key={href}>
-              <MenuLink isOpen={isMenuOpen} main={href === router.asPath} onClick={toggleMenuOpen}>
+              <MenuLink isOpen={isMenuOpen} main={href === router.asPath} onClick={handleCloseMenu}>
                 {linkText}
               </MenuLink>
             </Link>
