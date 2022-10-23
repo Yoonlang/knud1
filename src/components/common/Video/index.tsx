aa6a5a57bb42a602bd5a7bb515af3556f15c05b2
Climier-code <ikowhddus@naver.com>
2022-10-23 16:59:32 +0900

    feat: add Video Component

---
+++ src/components/common/Video/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useMount } from 'utils/useMount';
+
+interface Props {
+  src: string;
+}
+
+const Video: React.FC<Props> = ({ src }) => {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+
+  const cancelVideoMute = React.useCallback(() => {
+    if (videoRef.current?.muted === true) {
+      videoRef.current.muted = false;
+    }
+  }, []);
+
+  useMount(cancelVideoMute);
+
+  return (
+    <video ref={videoRef} autoPlay muted loop preload="metadata" controls>
+      <source src={src} type="video/mp4" />
+    </video>
+  );
+};
+
+export default Video;
